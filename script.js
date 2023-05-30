8fe619eb7128718970c27b58de1497d812d98663
Pablo Gal <pablo.gal@estudante.ufjf.br>
2023-05-30 19:36:41 -0300

    adiciona atualizacao no carregamento

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.forms.entrada;
         form.addEventListener('submit', envia)
 
         let estado = 0;
-
+        atualizar()
         function envia (evento) {
             evento.preventDefault()
             console.log('Formilário enviado!')
