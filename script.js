cbc48df900b45da9e321eae09e1e23aa8331c362
Pablo Gal <pablo.gal@estudante.ufjf.br>
2023-05-30 19:28:12 -0300

    adiciona estado basico

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const form = document.forms.entrada;
         form.addEventListener('submit', envia)
 
+        let estado = 0;
+
         function envia (evento) {
             evento.preventDefault()
             console.log('Formilário enviado!')
+            estado++
+            atualizar()
+        }
+
+        function atualizar() {
+            const ol = document.querySelector('ol')
+            ol.innerHTML = `<li>${estado}</li`
         }
